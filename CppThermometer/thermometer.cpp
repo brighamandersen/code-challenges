663ce5b497be1728798ea3b4ec0713c499c36352
Brigham Andersen <brighamband@gmail.com>
2021-07-02 23:51:12 -0600

    Finished thermostat

--- CppThermometer/thermometer.cpp
+++ CppThermometer/thermometer.cpp
@@ -1,34 +1,67 @@
-// Compile and run in one line using 'g++ thermometer.cpp && ./a.exe'
+// Compile and run in one line using 'g++ thermometer.cpp && ./a.exe' or './a.out'
 
 #include <iostream>
 #include <string>
+#include <limits>
 
-const int MIN_TEMP = 30.0f;
-const int MAX_TEMP = 100.0f;
-const int DEFAULT_TEMP = 76.0f;
+const int MIN_TEMPERATURE = 30.0f;
+const int MAX_TEMPERATURE = 100.0f;
+const int DEFAULT_TEMPERATURE = 76.0f;
 
 char getCharFromInput(std::istream&);
 float getNumberFromInput(std::istream&);
+std::string checkNewTemperature(float);
 void printThermostat(const float&, std::ostream&);
 
 int main() {
-    std::string currentFile = "";
-    std::string outputFileName = "";
-    std::string inputFile = "";
-    std::string outputFile = "";
-    std::string command = "";
-    float currentTemp = DEFAULT_TEMP;
-    float tempChange = 0.0f;
+    char command;
+    float currentTemperature = DEFAULT_TEMPERATURE;
+    float temperatureChange = 0.0f;
 
-    // std::ifstream is("input.txt");
-    // char c;
-    // while (is.get(c)) {
-    //     std::cout << c;
-    // }
+    // Welcome prompt
+    std::cout << "Hi, welcome to Cpp Thermostat!" << std::endl;
 
-    getCharFromInput(std::cin);
+    bool isOn = true;
+    while (isOn) {
+        std::cout << std::endl;
+        printThermostat(currentTemperature, std::cout);
 
-    printThermostat(currentTemp, std::cout);
+        std::cout << "Enter a command below:" << std::endl;
+        command = getCharFromInput(std::cin);
+
+        switch (command) {
+            case 'i': { 
+                    temperatureChange = getNumberFromInput(std::cin);
+                    std::string msg = checkNewTemperature(currentTemperature + temperatureChange);
+                    if (msg == "✅") {
+                        currentTemperature += temperatureChange;
+                    }
+                    std::cout << msg << std::endl;
+                }
+                break;
+            case 'd': {
+                    temperatureChange = getNumberFromInput(std::cin);
+                    std::string msg = checkNewTemperature(currentTemperature - temperatureChange);
+                    if (msg == "✅") {
+                        currentTemperature -= temperatureChange;
+                    }
+                    std::cout << msg << std::endl;
+                }
+                break;
+            case 'r':   
+                currentTemperature = DEFAULT_TEMPERATURE;
+                std::cout << "⏮" << std::endl;
+                break;
+            case 'o':
+                isOn = false;
+                std::cout << "⛔" << std::endl;
+                return 0; 
+            default:
+                std::cout << "❌ Invalid command, try again 🔄" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
 
     return 0;
 }
@@ -45,7 +78,16 @@ float getNumberFromInput(std::istream& in) {
     return f;
 }
 
-void printThermostat(const float& temp, std::ostream& out) {
-    out << "Temp is " << temp << std::endl;
-    out << "test\n";
+std::string checkNewTemperature(float newTemp) {
+    if (newTemp > MAX_TEMPERATURE) {
+        return "❌ Thermostat cannot exceed 100 ℉ 🔥";
+    }
+    if (newTemp < MIN_TEMPERATURE) {
+        return "❌ Thermostat cannot drop below 30 ℉ 🧊";
+    }
+    return "✅";
+}
+
+void printThermostat(const float& temperature, std::ostream& out) {
+    out << "Temperature is " << temperature << " ℉" << std::endl;
 }
