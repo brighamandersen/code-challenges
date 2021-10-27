dc9586250dba23f09b9f59e51bcab5b617b4dde4
Brigham Andersen <brighamband@gmail.com>
2021-10-27 09:49:46 -0600

    fixed cpp bubble sort

--- CppBubbleSort/bubble_sort.cpp
+++ CppBubbleSort/bubble_sort.cpp
@@ -2,39 +2,38 @@
 #include <string>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
-    // int numElements = 10;
-    // int A[numElements] = {5, 8, 2, 29, 4, 7, 1, 1321, 46, 6};
-    // int temp;
-    // bool didSwap = false;
+    int numElements = 10;
+    int A[numElements] = {5, 8, 2, 29, 4, 7, 1, 1321, 46, 6};
+    int temp;
+    bool didSwap = false;
 
-    // for (int i = 0; i < numElements; i++) {
-    //     if (i > 0) {
-    //         std::cout << ", ";
-    //     }
-    //     std::cout << A[i];
-    // }
-    // std::cout << "\n";
+    for (int i = 0; i < numElements; i++) {
+        if (i > 0) {
+            std::cout << ", ";
+        }
+        std::cout << A[i];
+    }
+    std::cout << "\n";
 
-    // do {
-    //     didSwap = false;
-    //     for (int i = 0; i <= (numElements - 2); i++) {
-    //         if (A[i] > A[i + 1]) {
-    //             didSwap = true;
-    //             temp = A[i];
-    //             A[i] = A[i + 1];
-    //             A[i + 1] = temp;
-    //         }
-    //     }
-    // } while (didSwap);
+    do {
+        didSwap = false;
+        for (int i = 0; i <= (numElements - 2); i++) {
+            if (A[i] > A[i + 1]) {
+                didSwap = true;
+                temp = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = temp;
+            }
+        }
+    } while (didSwap);
 
-    // for (int i = 0; i < numElements; i++) {
-    //     if (i > 0) {
-    //         std::cout << ", ";
-    //     }
-    //     std::cout << A[i];
-    // }
-    // std::cout << "\n";
+    for (int i = 0; i < numElements; i++) {
+        if (i > 0) {
+            std::cout << ", ";
+        }
+        std::cout << A[i];
+    }
+    std::cout << "\n";
 
     return 0;
 }
