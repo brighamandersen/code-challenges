971eb705141f1efcd7b8b2b75dc5dae9eab98575
Brigham Andersen <brighambandersen@gmail.com>
2024-06-07 23:46:40 -0600

    Fix spacing

--- palindrome1.py
+++ palindrome1.py
@@ -1,22 +1,24 @@
 def is_palindrome(word):
-	frontIndex = 0
-	backIndex = len(word) - 1
+    frontIndex = 0
+    backIndex = len(word) - 1
 
-	while (frontIndex < backIndex):
-		if word[frontIndex] != word[backIndex]:
-			return False
+    while (frontIndex < backIndex):
+        if word[frontIndex] != word[backIndex]:
+            return False
 
-		frontIndex += 1
-		backIndex -= 1
-	return True
+        frontIndex += 1
+        backIndex -= 1
+    return True
+
 
 def main():
     wordToCheck = "racecar"
 
     if is_palindrome(wordToCheck):
-    	print("Yes, it's a palindrome")
+        print("Yes, it's a palindrome")
     else:
-    	print("Nope, it's not a palindrome")
+        print("Nope, it's not a palindrome")
+
 
 if __name__ == "__main__":
     main()
